6bcdca7a3a63ed51eaceece1baef789a0a19ff48
aa-nikitin <a.nikitin91@mail.ru>
2019-02-08 10:55:11 +0300

    add getNews

--- server/controllers/getNews.js
+++ server/controllers/getNews.js
@@ -1,6 +1,10 @@
-// const passport = require('passport');
+const News = require('../models/news');
 
 module.exports = (req, res, next) => {
-  // console.log(req.body);
-  res.status(200).json({ user: req.user });
+  News.find({})
+    .populate('user')
+    .then(news => {
+      return res.status(200).json(news);
+    })
+    .catch(next);
 };
